429953bf9a843af2c0e6e0aa4c8ec81853fe7c39
lehieu1312 <hieulm1312@gmail.com>
2018-07-08 00:14:52 +0700

    complete

--- server/api.js
+++ server/api.js
@@ -5,7 +5,9 @@ var libData = require('../lib/data');
 
 router.get('/', (req, res) => {
     try {
+        // Lấy dữ liệu mẫu từ file data.js
         var dataTransaction = libData.transactionPrice;
+        // Trả về dữ liệu mẫu từ file data.json
         res.json({ status: 200, msg: "Success", dataTransaction: dataTransaction });
     } catch (error) {
         res.json({ status: 201, msg: error + '' });
